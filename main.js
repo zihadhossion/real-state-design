7b29077dead64f798601fe0fda93f915803dc16f
zihadhossion <zihadhossion2005@gmail.com>
2023-10-08 23:47:28 +0600

    sidebar menu addded

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const subMenu = document.querySelectorAll(".side-content .list-item");
 
 for (const ele of subMenu) {
     ele.addEventListener("click", () => {
+        const rightAero = document.querySelector("#sidebar .fa-chevron-right");
         const ssMenu = ele.nextElementSibling;
-        ssMenu.style.transform = "scaleY(1)";
-        ele.parentElement.style.height = "10vh";
+        ssMenu.classList.toggle("ss-menu");
+        rightAero.style.transform = "rotate(90deg)";
     })
 }
 
