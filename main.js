53c40008e21e8a98d5b85829c859caf4fe3dce2c
zihadhossion <zihadhossion2005@gmail.com>
2023-10-13 23:57:41 +0600

    slider add

--- main.js
+++ main.js
@@ -52,3 +52,17 @@ for (const btn of btnActive) {
         btn.classList.add("btn-active");
     })
 }
+//Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
